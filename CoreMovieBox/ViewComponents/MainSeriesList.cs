0718006cd1a6a84fb4da991a3995742715a9b102
atakan18 <atakankasbasi@gmail.com>
2023-03-13 22:40:43 +0300

    new

---
+++ CoreMovieBox/ViewComponents/MainSeriesList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMovieBox.ViewComponents
+{
+    public class MainSeriesList:ViewComponent
+    {
+        TvSerieManager tvSerieManager = new TvSerieManager(new EfTvSerieDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = tvSerieManager.TGetList();
+            return View(values);
+        }
+    }
+}
