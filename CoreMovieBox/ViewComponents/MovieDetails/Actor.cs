fa8e356a0a603986f8c3885adc7431f502b1cb4a
Muhammet-Dobaoglu <muhammetdobaoglu@hotmail.com>
2023-03-16 16:29:00 +0300

    Add

---
+++ CoreMovieBox/ViewComponents/MovieDetails/Actor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMovieBox.ViewComponents.MovieDetails
+{
+	public class Actor : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ CoreMovieBox/ViewComponents/MovieDetails/Testimonial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMovieBox.ViewComponents.MovieDetails
+{
+	public class Testimonial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
